a160b3e03ee818ea50dd74eb9028a9d6a67ea78f
25rae25 <whdudfo0124@gmail.com>
2023-01-04 16:27:42 +0900

    MemberController - member import오류?

--- src/main/java/controller/MemberController.java
+++ src/main/java/controller/MemberController.java
@@ -3,7 +3,10 @@ package controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import service.MemberService;
+
+import java.lang.reflect.Member;
 
 @Controller
 public class MemberController {
@@ -20,4 +23,15 @@ public class MemberController {
         return "members/createMembersForm";
 
     }
+
+    // member 왜 에러나는거지?
+//    @PostMapping("/members/new")
+//    public String create(MemberForm form) {
+//        Member member = new Member();
+//        member.setName(form.getName());
+//
+//        memberService.join(member);
+//
+//        return "redirect:/";
+//    }
 }
